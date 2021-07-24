d52bb7c85026c6113c927c65f78e8ef29d379bdc
Folusho Oladipo <fdoladipo@gmail.com>
2021-07-24 21:35:40 +0100

    Add a page that lists all articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
   get "about", to: "pages#about"
+
+  # This is a shorthand that makes Rails generate a bunch of routes for
+  # the model specified. That way, we can focus on writing the controller actions
+  # that handle requests sent to those routes. For example, Rails will generate:
+  # - GET /articles,
+  # - POST /articles,
+  # - GET /articles/:id (a read-only page for viewing the article),
+  # - GET /articles/:id/edit (the page with a form for editing the article),
+  # - PUT or PATCH /articles/:id,
+  # - DELETE /articles/:id
+  # - etc
+  # if we simply write `resources :articles`. To see the full list of generated routes,
+  # run `rails routes --expanded` in a terminal. However, we can use `:only` to specify
+  # the exact routes we want to be auto-generated.
+  resources :articles, only: [:index]
 end
